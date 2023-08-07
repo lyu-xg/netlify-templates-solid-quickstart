f26f8e62d03044ff7693ffbd74d1e9495c138223
Xueguang <lu.xue@northeastern.edu>
2023-08-08 00:40:35 +0800

    manual update

--- src/root.tsx
+++ src/root.tsx
@@ -18,8 +18,8 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
+        <Meta charset="UTF-8" />
         <Title>微信｜WeChat</Title>
-        <Meta charset="utf-8" />
         <Meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
